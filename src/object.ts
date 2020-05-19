8a808e089a9a4ada95da94844a85e76d3b264655
rui-watanabe <wataru610806@gmail.com>
2020-05-19 18:37:56 +0900

    first object

---
+++ src/object.ts
@@ -0,0 +1,9 @@
+export {};
+
+let profile1: object = { name: 'Max'};
+profile1 = { birthYear: 1992 };
+
+let profile2: { name: string } = { name: 'Max'};
+// profile2 = { birthYear: 1992 };
+// profile2 = { name: 1992 };
+
