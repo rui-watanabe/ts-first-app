71d5d8dbc8d46a592ac4bf671e8f95fdc010a725
rui-watanabe <wataru610806@gmail.com>
2020-05-20 20:11:52 +0900

    first interface

---
+++ src/interfaces.ts
@@ -0,0 +1,21 @@
+export {};
+
+type ObjectType = { 
+  name: string; 
+  age: number;
+} 
+
+interface ObjectInterface {
+  name: string; 
+  age: number;
+}
+
+// let object:ObjectType = {
+//   name: 'max',
+//   age: 22
+// }
+
+let object:ObjectInterface = {
+  name: 'max',
+  age: 22
+}
