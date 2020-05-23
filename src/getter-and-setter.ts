46d6eea8c5a7ce8310d301385e96da6575f32f7e
rui-watanabe <wataru610806@gmail.com>
2020-05-23 21:38:25 +0900

    getter and setter

---
+++ src/getter-and-setter.ts
@@ -0,0 +1,46 @@
+export {};
+
+// * owner
+//    * 所有者
+//    * 初期化時に設定できる
+//    * 途中で変更できない
+//    * 参照できる
+// * secretNumber
+//    * 所有者
+//    * 初期化時に設定できる
+//    * 途中で変更できる
+//    * 参照できない
+
+
+
+class MyNumberCard {
+  private _owner: string;
+  private _secretNumber: number;
+
+  constructor(owner: string, secretNumber: number) {
+    this._owner = owner;
+    this._secretNumber = secretNumber;
+  }
+
+  get owner() {
+    return this._owner;
+  }
+
+  set secretNumber(secretNumber: number) {
+    this._secretNumber = secretNumber;
+  }
+
+  debugPrint(){
+    return `secretNumber: ${this._secretNumber}`;
+  }
+}
+
+let card = new MyNumberCard('max', 123456789);
+console.log(card.debugPrint());
+card.secretNumber = 111111111;
+console.log(card.debugPrint());
+// card.owner = 'Max';
+console.log(card.owner)
+// card.secretNumber;
+// card._secretNumber;
+console.log(card.secretNumber);
