ede75e395fa4f4f5a9c9c19dabc187cc24e0f5e4
rui-watanabe <wataru610806@gmail.com>
2020-05-25 21:31:38 +0900

    type interface and type annotation

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export {};
+
+let hasValue = true;
+let count = 10;
+let float = 3.14;
+let negative = -0.12;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+let hello: string;
+hello = 'hello';
